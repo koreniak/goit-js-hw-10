8171e298ca48f52c141a568192a77ee67a7fe863
Borys Koreniak <koreniak.borys@gmail.com>
2023-04-25 23:25:50 +0300

    Зробив дооформлення, винес ще одну функцію

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,9 @@
 export function fetchCounties(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(
-        resp => {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(resp => {
             if (!resp.ok) {
                 throw new Error(resp.ok);
             }
                 return resp.json();
-        }
-    );
+        });
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import { fetchCounties } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-const debounce = require('lodash.debounce')
+const debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 
@@ -11,55 +11,61 @@ const refs = {
     infoField: document.querySelector('.country-info')
 }
 
-refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-    const inputValue = e.target.value
-    const trimInputValue = inputValue.trim()
+    const inputValue = e.target.value;
+    const trimInputValue = inputValue.trim();
     
     if (trimInputValue) {
-        clearCountriesInfo()
-        getCountryInfo(trimInputValue)
-    }
-}
+        clearCountriesInfo();
+        getCountryInfo(trimInputValue);
+    };
+};
 
 function getCountryInfo(countriesResp) {
     fetchCounties(countriesResp)
         .then((countries) => {
-            if (countries.length > 10) {
-                Notify.info('Too many matches found. Please enter a more specific name.')
-            } else if (countries.length >= 2) {
-                renderCountriesListMarkup(countries)
-            } else {
-                renderCountriesInfoMarkup(countries)
-            }
-    }).catch((error) => Notify.failure('Oops, there is no country with that name'))
-}
+            serchCountries(countries)
+        }).catch((error) => Notify.failure('Oops, there is no country with that name'));
+};
+
+function serchCountries(countriesData) {
+    if (countriesData.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (countriesData.length >= 2) {
+        renderCountriesListMarkup(countriesData);
+    } else {
+        renderCountryInfoMarkup(countriesData);
+    };
+};
 
 function renderCountriesListMarkup(countriesArray){
     const markup = countriesArray.map(({flags, name}) => {
         return `<li class="country-item">
-                    <img src="${flags.svg}" alt="${flags.alt}" width="60" />
-                    <p class="name-country">${name.official}</p>
-                </li>`
+                    <img src="${flags.svg}" alt="${flags.alt}" width="80" />
+                    <p class="country-name">${name.official}</p>
+                </li>`;
     }).join('');
     refs.list.insertAdjacentHTML('beforeend', markup);
 }
 
-function renderCountriesInfoMarkup(country){
+function renderCountryInfoMarkup(country){
     const markup = country.map(({flags, name, capital, population, languages}) => {
-        return `<div class="country-title">
-                    <img src="${flags.svg}" alt="${flags.alt}" width="160" />
-                    <h1 class="title-country">${name.official}</h1>
+        return `<div class="country-head">
+                    <img src="${flags.svg}" alt="${flags.alt}" width="80" />
+                    <h1 class="country-title">${name.official}</h1>
                 </div>
-                <p><span class="country-prop">Capital</span>: ${capital}</p>
-                <p><span class="country-prop">Population</span>: ${population}</p>
-                <p><span class="country-prop">Languages</span>: ${Object.values(languages).join(", ")}</p>`
+                <ul>
+                    <li><span class="country-prop">Capital</span>: ${capital}</li>
+                    <li><span class="country-prop">Population</span>: ${population}</li>
+                    <li><span class="country-prop">Languages</span>: ${Object.values(languages).join(', ')}</li>
+                </ul>`;
     }).join('');
     refs.infoField.insertAdjacentHTML('beforeend', markup);
 }
 
-function clearCountriesInfo(){
+function clearCountriesInfo() {
     refs.list.innerHTML = '';
     refs.infoField.innerHTML = '';
 }
